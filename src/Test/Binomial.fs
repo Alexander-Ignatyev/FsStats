2a8c136ef2b19786064724c07268c00e3ccf4d5f
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-05-09 21:39:17 +0100

    add FsUnit package

--- src/Test/Binomial.fs
+++ src/Test/Binomial.fs
@@ -1,18 +1,19 @@
 module Fsharp.Statistics.Binomial.Tests
 
 open Xunit
+open FsUnit.Xunit
 
 open Fsharp.Statistics.Binomial
 
 
 [<Fact>]
 let ``10 choose 3 equals 120`` () =
-    Assert.Equal(120, (coefficient 10 3))
+    coefficient 10 3 |> should equal 120
 
 [<Fact>]
 let ``4 choose 2 equals 6`` () =
-    Assert.Equal(6, coefficient 4 2)
+    coefficient 4 2 |> should equal 6
 
 [<Fact>]
 let ``8 choose 5 equals 70`` () =
-    Assert.Equal(70, coefficient 8 4)
+    coefficient 8 4 |> should equal 70
