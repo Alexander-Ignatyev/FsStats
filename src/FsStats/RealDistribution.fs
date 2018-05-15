9c2b841e29403817fe80b8602e00fdfdaac6148f
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-05-15 01:19:08 +0100

    Introduce RealDistribution abstract class; inherit NormalDistribution from it; and implemented unit tests of Hypothesis testing of RealDistribution

---
+++ src/FsStats/RealDistribution.fs
@@ -0,0 +1,12 @@
+module FsStats.RealDistribution
+
+open FsStats.Distribution
+
+
+[<AbstractClass>]
+type RealDistribution() =
+    inherit Distribution<float>()
+    // There is no well-defined Probability Mass Function (PMF) for Real distrinution
+    // so Cumulative Distribution Function is the same as PMF.
+    override this.CumulativeProbability x = this.Probability x
+    override this.CumulativeProbability (m, n) = this.CumulativeProbability n - this.CumulativeProbability m
