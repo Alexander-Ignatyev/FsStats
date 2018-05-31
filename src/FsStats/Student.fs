8a22bbba8fcd22508e701992718ab593ab86a228
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-06-01 00:12:56 +0100

    Student's CDF

---
+++ src/FsStats/Student.fs
@@ -0,0 +1,14 @@
+namespace FsStats
+
+
+/// Student's t-distribution
+module Student =
+
+    /// Cumulative distribution function
+    /// takes degrees of freedom and t score
+    let rec cdf df t =
+        if t < 0.0 then 1.0 - cdf df -t
+        else 
+            let nu = float df
+            let x = nu / (t * t + nu)
+            1.0 - 0.5 * Special.betainc (nu * 0.5) 0.5 x
