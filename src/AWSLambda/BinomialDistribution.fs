38dd44c93f48fe99b4fba1c699e569f226b5e37a
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-06-16 09:04:27 +0100

    AWS Lambda: added Binomial Distribution

---
+++ src/AWSLambda/BinomialDistribution.fs
@@ -0,0 +1,41 @@
+﻿namespace FsStats.AWSLambda
+
+open FsStats
+
+module BinomialDistribution =
+    type Request = {
+        Params : BinomialDistribution.T
+        Curve : int option
+        Pmf : int option
+        Cdf : int option
+        Sample : int option
+    }
+
+    type Response = {
+        Params : BinomialDistribution.T
+        Mean : float
+        StdDev : float
+        Variance : float
+        Curve : (int[] * float[]) option
+        Pmf : float option
+        Cdf : float option
+        Sample : int[] option
+    }
+
+    let handleCurve (d : BinomialDistribution.T) n =
+        let x = [| for x in 0 .. n -> x |]
+        let y = Array.map (BinomialDistribution.pmf d) x
+        (x, y)
+
+    let rnd = System.Random()
+
+    let handle (r: Request) = {
+            Params = r.Params
+            Mean = BinomialDistribution.mean r.Params
+            StdDev = BinomialDistribution.stddev r.Params
+            Variance = BinomialDistribution.variance r.Params
+            Curve = Option.map (handleCurve r.Params) r.Curve
+            Pmf = Option.map (BinomialDistribution.pmf r.Params) r.Pmf
+            Cdf = Option.map (BinomialDistribution.cdf r.Params) r.Cdf
+            Sample = Option.map (BinomialDistribution.sample r.Params rnd) r.Sample
+        }
--- src/AWSLambda/Processor.fs
+++ src/AWSLambda/Processor.fs
@@ -3,15 +3,18 @@
 module Processor =
     type Request = {
         Bernoulli : BernoulliDistribution.Request option
+        Binomial : BinomialDistribution.Request option
         Normal : NormalDistribution.Request option
     }
 
     type Response = {
         Bernoulli : BernoulliDistribution.Response option
+        Binomial : BinomialDistribution.Response option
         Normal : NormalDistribution.Response option
     }
     
     let handle (r: Request) = {
         Bernoulli = Option.map BernoulliDistribution.handle r.Bernoulli
+        Binomial = Option.map BinomialDistribution.handle r.Binomial
         Normal = Option.map NormalDistribution.handle r.Normal
     }
