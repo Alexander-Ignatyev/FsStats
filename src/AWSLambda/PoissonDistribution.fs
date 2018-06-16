2a24343fc3f0af91755d608bc350f212f6612835
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-06-16 09:29:05 +0100

    AWS Lambda: added Poisson Distribution

---
+++ src/AWSLambda/PoissonDistribution.fs
@@ -0,0 +1,41 @@
+﻿namespace FsStats.AWSLambda
+
+open FsStats
+
+module PoissonDistribution =
+    type Request = {
+        Params : PoissonDistribution.T
+        Curve : int option
+        Pmf : int option
+        Cdf : int option
+        Sample : int option
+    }
+
+    type Response = {
+        Params : PoissonDistribution.T
+        Mean : float
+        StdDev : float
+        Variance : float
+        Curve : (int[] * float[]) option
+        Pmf : float option
+        Cdf : float option
+        Sample : int[] option
+    }
+
+    let handleCurve (d : PoissonDistribution.T) n =
+        let x = [| for x in 0 .. n -> x |]
+        let y = Array.map (PoissonDistribution.pmf d) x
+        (x, y)
+
+    let rnd = System.Random()
+
+    let handle (r: Request) = {
+            Params = r.Params
+            Mean = PoissonDistribution.mean r.Params
+            StdDev = PoissonDistribution.stddev r.Params
+            Variance = PoissonDistribution.variance r.Params
+            Curve = Option.map (handleCurve r.Params) r.Curve
+            Pmf = Option.map (PoissonDistribution.pmf r.Params) r.Pmf
+            Cdf = Option.map (PoissonDistribution.cdf r.Params) r.Cdf
+            Sample = Option.map (PoissonDistribution.sample r.Params rnd) r.Sample
+        }
--- src/AWSLambda/Processor.fs
+++ src/AWSLambda/Processor.fs
@@ -4,17 +4,20 @@ module Processor =
     type Request = {
         Bernoulli : BernoulliDistribution.Request option
         Binomial : BinomialDistribution.Request option
+        Poisson : PoissonDistribution.Request option
         Normal : NormalDistribution.Request option
     }
 
     type Response = {
         Bernoulli : BernoulliDistribution.Response option
         Binomial : BinomialDistribution.Response option
+        Poisson : PoissonDistribution.Response option
         Normal : NormalDistribution.Response option
     }
     
     let handle (r: Request) = {
         Bernoulli = Option.map BernoulliDistribution.handle r.Bernoulli
         Binomial = Option.map BinomialDistribution.handle r.Binomial
+        Poisson = Option.map PoissonDistribution.handle r.Poisson
         Normal = Option.map NormalDistribution.handle r.Normal
     }
