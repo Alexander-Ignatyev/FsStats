cd931b289ddca5f1fb95595c4940fb0afdc531b3
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2018-06-27 21:55:42 +0100

    AMWS Lambda: unified OneSampleMeanTest for Z-Test and t-Test

--- src/AWSLambda/Hypothesis.fs
+++ src/AWSLambda/Hypothesis.fs
@@ -3,25 +3,17 @@
 open FsStats
 
 module Hypothesis =
-    type OneSampleZTest = {
+    type OneSampleMeanTest = {
         TrueMean : float
-        TrueStdDev : float
+        StdDev : float   // Population std dev for Z-Test or Sample std dev for t-Test
         SampleMean : float
-        SampleSize : int
-        TestType : Hypothesis.TestType
-    }
-
-    type OneSampleTTest = {
-        TrueMean : float
-        SampleMean : float
-        SampleStdDev : float
         SampleSize : int
         TestType : Hypothesis.TestType
     }
 
     type Request = {
-        OneSampleZTest : OneSampleZTest option
-        OneSampleTTest : OneSampleTTest option
+        OneSampleZTest : OneSampleMeanTest option
+        OneSampleTTest : OneSampleMeanTest option
     }
 
     type Response = {
@@ -29,10 +21,10 @@ module Hypothesis =
         OneSampleTTest : float option
     }
 
-    let doOneSampleZTest {TrueMean = mu; TrueStdDev = std; SampleMean = mean; SampleSize = size; TestType = testType} =
+    let doOneSampleZTest {TrueMean = mu; StdDev = std; SampleMean = mean; SampleSize = size; TestType = testType} =
         Hypothesis.OneSample.zTest mu std mean size testType
 
-    let doOneSampleTTest {TrueMean = mu; SampleMean = mean; SampleStdDev = std; SampleSize = size; TestType = testType} =
+    let doOneSampleTTest {TrueMean = mu; StdDev = std; SampleMean = mean; SampleSize = size; TestType = testType} =
         Hypothesis.OneSample.tTest mu mean std size testType
 
     let handle (r: Request) = {
